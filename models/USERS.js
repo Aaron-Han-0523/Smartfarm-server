609e8fa75b1e6d9fddc6340057b8eb87e91e9631
HANW001 <mosadi@nate.com>
2021-12-28 11:47:20 +0900

    add nodejs

---
+++ models/USERS.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('USERS', {
+            uid: {
+                type: DataTypes.STRING(20),
+                primaryKey: true
+            },
+            password: {
+                type: DataTypes.STRING(255)
+            },
+            sid_base: {
+                type:DataTypes.STRING(20)
+            }
+        },
+        {
+            createdAt: false,
+            updatedAt: false,
+            tableName: "USERS"
+        });
+};
+
+//https://loy124.tistory.com/373
+//https://devlog-wjdrbs96.tistory.com/221
