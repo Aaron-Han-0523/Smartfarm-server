d8059406b987d59e96697bfcbcbd8fd1b6f18193
markseeun <mark233891@gmaiil.com>
2021-12-31 13:57:39 +0900

    [+] trends table

--- models/index.js
+++ models/index.js
@@ -58,6 +58,8 @@ db.Sequelize = Sequelize;
 db.User = require('./USERS')(sequelize, Sequelize);
 db.Sites = require('./SITES')(sequelize, Sequelize);
 db.Sensors = require('./SENSORS')(sequelize, Sequelize);
+db.Trends = require('./TRENDS')(sequelize, Sequelize);
+
 
 
 module.exports = db;
