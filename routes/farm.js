6fb830920b5345aba115c86937146f69d50ee2fd
HANW001 <mosadi@nate.com>
2022-01-06 15:59:17 +0900

    KWHY-98 [+] add logout session

--- routes/farm.js
+++ routes/farm.js
@@ -10,32 +10,14 @@ const stringify = require("json-stringify-pretty-compact"); //json 값을 문자
 
 let router = express.Router();
 
-const {
-  User,
-  Trends,
-  Motors
-} = require("../models");
-const {
-  Sites
-} = require("../models");
-const {
-  Sensors
-} = require("../models");
-const {
-  Cctvs
-} = require("../models");
-const {
-  Events
-} = require("../models");
-const {
-  Pumps
-} = require("../models");
-const {
-  Valves
-} = require("../models");
-const {
-  Actuators
-} = require("../models");
+const { User, Trends, Motors } = require("../models");
+const { Sites } = require("../models");
+const { Sensors } = require("../models");
+const { Cctvs } = require("../models");
+const { Events } = require("../models");
+const { Pumps } = require("../models");
+const { Valves } = require("../models");
+const { Actuators } = require("../models");
 
 // testimport DB // king
 
@@ -64,10 +46,10 @@ router.post("/login", (req, res, next) => {
   let password = req.body.password;
   if (!empty(userId) && !empty(password)) {
     User.findOne({
-        where: {
-          uid: userId,
-        },
-      })
+      where: {
+        uid: userId,
+      },
+    })
       .then((results) => {
         if (!results) {
           res.json({
@@ -75,7 +57,7 @@ router.post("/login", (req, res, next) => {
             error: null,
             data: null,
           });
-          console.log('아이디가 없음');
+          console.log("아이디가 없음");
           // res.status(400).send("error");
         }
         bcrypt.compare(password, results.password, (error, result) => {
@@ -113,6 +95,34 @@ router.post("/login", (req, res, next) => {
   }
 });
 
+router.route("/logout").get(
+  //설정된 쿠키정보를 본다
+  function (req, res) {
+    console.log("/loginout 라우팅 함수호출 됨");
+
+    if (req.session.loginData) {
+      console.log("로그아웃 처리");
+      req.session.destroy(function (err) {
+        if (err) {
+          console.log("세션 삭제시 에러");
+        }
+        console.log("세션 삭제 성공");
+        res.json({
+          results,
+          result: true,
+        });
+      }); //세션정보 삭제
+    } else {
+      console.log("로긴 안되어 있음");
+      res.json({
+        result: false,
+        error: null,
+        data: null,
+      });
+    }
+  }
+);
+
 router.get("/account", async (req, res, next) => {
   User.findAll({})
     .then((result) => {
@@ -136,10 +146,10 @@ router.post("/account", async (req, res, next) => {
     bcrypt.hash(password, saltRounds, (error, hash) => {
       password = hash;
       User.create({
-          uid: uid,
-          password: password,
-          sid: sid,
-        })
+        uid: uid,
+        password: password,
+        sid: sid,
+      })
         .then((result) => {
           res.json({
             result: result,
@@ -167,10 +177,10 @@ router.post("/account", async (req, res, next) => {
 
 router.get("/:userId", async (req, res, next) => {
   User.findAll({
-      where: {
-        uid: req.params.userId,
-      },
-    })
+    where: {
+      uid: req.params.userId,
+    },
+  })
     .then((result) => {
       // res.json({"data":result, test: "test", error: null})
       res.json(result);
@@ -187,14 +197,17 @@ router.put("/:userId", async (req, res, next) => {
   let uid = req.body.uid;
   let sid = req.body.sid_base;
   if (!empty(req.params.userId)) {
-    User.update({
+    User.update(
+      {
         uid: uid,
         sid: sid,
-      }, {
+      },
+      {
         where: {
           uid: req.params.userId,
         },
-      })
+      }
+    )
       .then((result) => {
         res.json({
           result: result,
@@ -219,15 +232,15 @@ router.put("/:userId", async (req, res, next) => {
   }
 });
 
-// 현재 비밀번호 확인 
+// 현재 비밀번호 확인
 router.post("/login/:userId/checkpw", async (req, res, next) => {
   let password = req.body.password;
   if (!empty(password)) {
     User.findOne({
-        where: {
-          uid: req.params.userId,
-        },
-      })
+      where: {
+        uid: req.params.userId,
+      },
+    })
       .then((results) => {
         bcrypt.compare(password, results.password, (error, result) => {
           if (result) {
@@ -263,19 +276,20 @@ router.post("/login/:userId/checkpw", async (req, res, next) => {
   }
 });
 
-
-
 router.put("/:userId/password", async (req, res, next) => {
   let password = req.body.password;
   bcrypt.hash(password, saltRounds, (error, hash) => {
     password = hash;
-    User.update({
+    User.update(
+      {
         password: password,
-      }, {
+      },
+      {
         where: {
           uid: req.params.userId,
         },
-      })
+      }
+    )
       .then((result) => {
         res.json({
           result: result,
@@ -297,10 +311,10 @@ router.put("/:userId/password", async (req, res, next) => {
 router.delete("/:userId", async (req, res, next) => {
   if (!empty(req.params.userId)) {
     User.destroy({
-        where: {
-          uid: req.params.userId,
-        },
-      })
+      where: {
+        uid: req.params.userId,
+      },
+    })
       .then((result) => {
         res.json(result);
       })
@@ -352,25 +366,25 @@ router.post("/:userId/sites", async (req, res, next) => {
 
   if (!empty(req.params.userId)) {
     Sites.create({
-        sid: sid,
-        uid: req.params.userId,
-        site_name: site_name,
-        site_address: site_address,
-        site_gps_latitude: site_gps_latitude,
-        site_gps_longitude: site_gps_longitude,
-        site_th_sensor_count: site_th_sensor_count,
-        site_soil_sensor_count: site_soil_sensor_count,
-        site_side_motor_count: site_side_motor_count,
-        site_top_motor_count: site_top_motor_count,
-        site_actuator_count: site_actuator_count,
-        site_pump_count: site_pump_count,
-        site_valve_count: site_valve_count,
-        site_cctv_count: site_cctv_count,
-        site_set_alarm_enable: site_set_alarm_enable,
-        site_set_alarm_high: site_set_alarm_high,
-        site_set_alarm_low: site_set_alarm_low,
-        site_set_alarm_timer: site_set_alarm_timer,
-      })
+      sid: sid,
+      uid: req.params.userId,
+      site_name: site_name,
+      site_address: site_address,
+      site_gps_latitude: site_gps_latitude,
+      site_gps_longitude: site_gps_longitude,
+      site_th_sensor_count: site_th_sensor_count,
+      site_soil_sensor_count: site_soil_sensor_count,
+      site_side_motor_count: site_side_motor_count,
+      site_top_motor_count: site_top_motor_count,
+      site_actuator_count: site_actuator_count,
+      site_pump_count: site_pump_count,
+      site_valve_count: site_valve_count,
+      site_cctv_count: site_cctv_count,
+      site_set_alarm_enable: site_set_alarm_enable,
+      site_set_alarm_high: site_set_alarm_high,
+      site_set_alarm_low: site_set_alarm_low,
+      site_set_alarm_timer: site_set_alarm_timer,
+    })
       .then((result) => {
         res.json({
           result: result,
@@ -397,11 +411,11 @@ router.post("/:userId/sites", async (req, res, next) => {
 
 router.get("/:userId/sites/:siteId", async (req, res, next) => {
   Sites.findAll({
-      where: {
-        uid: req.params.userId,
-        sid: req.params.siteId,
-      },
-    })
+    where: {
+      uid: req.params.userId,
+      sid: req.params.siteId,
+    },
+  })
     .then((result) => {
       // res.json({"data":result, test: "test", error: null})
       res.json(result);
@@ -417,11 +431,11 @@ router.get("/:userId/sites/:siteId", async (req, res, next) => {
 router.delete("/:userId/sites/:siteId", async (req, res, next) => {
   if (!empty(req.params.userId)) {
     Sites.destroy({
-        where: {
-          uid: req.params.userId,
-          sid: req.params.siteId,
-        },
-      })
+      where: {
+        uid: req.params.userId,
+        sid: req.params.siteId,
+      },
+    })
       .then((result) => {
         res.json(result);
       })
@@ -455,7 +469,8 @@ router.put("/:userId/sites/:siteId", async (req, res, next) => {
   let site_set_alarm_low = req.body.site_set_alarm_low;
   let site_set_alarm_timer = req.body.site_set_alarm_timer;
   if (!empty(req.params.siteId)) {
-    Sites.update({
+    Sites.update(
+      {
         site_name: site_name,
         site_address: site_address,
         site_gps_latitude: site_gps_latitude,
@@ -472,12 +487,14 @@ router.put("/:userId/sites/:siteId", async (req, res, next) => {
         site_set_alarm_high: site_set_alarm_high,
         site_set_alarm_low: site_set_alarm_low,
         site_set_alarm_timer: site_set_alarm_timer,
-      }, {
+      },
+      {
         where: {
           uid: req.params.userId,
           sid: req.params.siteId,
         },
-      })
+      }
+    )
       .then((result) => {
         res.json(result);
       })
@@ -520,12 +537,12 @@ router.get(
   "/:userId/site/:siteId/sensors/:sensorId",
   async (req, res, next) => {
     Sensors.findAll({
-        where: {
-          uid: req.params.userId,
-          sid: req.params.siteId,
-          sensor_id: req.params.sensorId,
-        },
-      })
+      where: {
+        uid: req.params.userId,
+        sid: req.params.siteId,
+        sensor_id: req.params.sensorId,
+      },
+    })
       .then((result) => {
         res.json({
           data: result,
@@ -546,13 +563,13 @@ router.get(
   "/:userId/site/:siteId/sensors/:sensorId/trends",
   async (req, res, next) => {
     Trends.findAll({
-        attributes: ["time_stamp", "value"],
-        where: {
-          uid: req.params.userId,
-          sid: req.params.siteId,
-          sensor_id: req.params.sensorId,
-        },
-      })
+      attributes: ["time_stamp", "value"],
+      where: {
+        uid: req.params.userId,
+        sid: req.params.siteId,
+        sensor_id: req.params.sensorId,
+      },
+    })
       .then((result) => {
         res.json({
           data: result,
@@ -574,16 +591,15 @@ router.get(
 
   async (req, res, next) => {
     Motors.findAll({
-        where: {
-          uid: req.params.userId,
-          sid: req.params.siteId,
-          motor_type: "side",
-        },
-      })
+      where: {
+        uid: req.params.userId,
+        sid: req.params.siteId,
+        motor_type: "side",
+      },
+    })
       .then((result) => {
         res.json({
           data: result,
-
         });
       })
       .catch((err) => {
@@ -601,16 +617,19 @@ router.put(
   async (req, res, next) => {
     let motor_type = req.body.motor_type;
     let motor_name = req.body.motor_name;
-    Motors.update({
+    Motors.update(
+      {
         motor_type: motor_type,
         motor_name: motor_name,
-      }, {
+      },
+      {
         where: {
           uid: req.params.userId,
           sid: req.params.siteId,
           motor_type: "side",
         },
-      })
+      }
+    )
       .then((result) => {
         res.json({
           data: result,
@@ -632,17 +651,20 @@ router.put(
   async (req, res, next) => {
     // let motor_type = req.body.motor_type;
     let motor_name = req.body.motor_name;
-    Motors.update({
+    Motors.update(
+      {
         // motor_type: motor_type,
         motor_name: motor_name,
-      }, {
+      },
+      {
         where: {
           uid: req.params.userId,
           sid: req.params.siteId,
           motor_id: req.params.motorId,
           motor_type: "side",
         },
-      })
+      }
+    )
       .then((result) => {
         res.json({
           data: result,
@@ -664,12 +686,12 @@ router.get(
   async (req, res, next) => {
     if (!empty(req.params.userId)) {
       Motors.findAll({
-          where: {
-            uid: req.params.userId,
-            sid: req.params.siteId,
-            motor_type: "top",
-          },
-        })
+        where: {
+          uid: req.params.userId,
+          sid: req.params.siteId,
+          motor_type: "top",
+        },
+      })
         .then((result) => {
           res.json({
             data: result,
@@ -699,11 +721,11 @@ router.get(
 
 router.get("/:userId/site/:siteId/controls/valves", async (req, res, next) => {
   Valves.findAll({
-      where: {
-        uid: req.params.userId,
-        sid: req.params.siteId,
-      },
-    })
+    where: {
+      uid: req.params.userId,
+      sid: req.params.siteId,
+    },
+  })
     .then((result) => {
       // res.json({"data":result, test: "test", error: null})
       res.json(result);
@@ -723,17 +745,20 @@ router.put(
     let valve_type = req.body.valve_type;
     let valve_name = req.body.valve_name;
     if (!empty(req.params.valvesId)) {
-      Valves.update({
+      Valves.update(
+        {
           valve_id: valve_id,
           valve_type: valve_type,
           valve_name: valve_name,
-        }, {
+        },
+        {
           where: {
             uid: req.params.userId,
             sid: req.params.siteId,
             valve_id: req.params.valvesId,
           },
-        })
+        }
+      )
         .then((result) => {
           res.json(result);
         })
@@ -763,11 +788,11 @@ router.put(
 
 router.get("/:userId/site/:siteId/cctvs", async (req, res, next) => {
   Cctvs.findAll({
-      where: {
-        uid: req.params.userId,
-        sid: req.params.siteId,
-      },
-    })
+    where: {
+      uid: req.params.userId,
+      sid: req.params.siteId,
+    },
+  })
     .then((result) => {
       // res.json({"data":result, test: "test", error: null})
       res.json(result);
@@ -788,18 +813,21 @@ router.put(
     let cctv_name = req.body.cctv_name;
     let cctv_url = req.body.cctv_url;
     if (!empty(req.params.cctvId)) {
-      Cctvs.update({
+      Cctvs.update(
+        {
           cctv_id: cctv_id,
           cctv_type: cctv_type,
           cctv_name: cctv_name,
           cctv_url: cctv_url,
-        }, {
+        },
+        {
           where: {
             uid: req.params.userId,
             sid: req.params.siteId,
             cctv_id: req.params.cctvId,
           },
-        })
+        }
+      )
         .then((result) => {
           res.json(result);
         })
@@ -829,11 +857,11 @@ router.put(
 
 router.get("/:userId/site/:siteId/settings", async (req, res, next) => {
   Events.findAll({
-      where: {
-        uid: req.params.userId,
-        sid: req.params.siteId,
-      },
-    })
+    where: {
+      uid: req.params.userId,
+      sid: req.params.siteId,
+    },
+  })
     .then((result) => {
       // res.json({"data":result, test: "test", error: null})
       res.json(result);
@@ -852,17 +880,20 @@ router.put("/:userId/site/:siteId/settings", async (req, res, next) => {
   let site_set_alarm_low = req.body.site_set_alarm_low;
   let site_set_alarm_timer = req.body.site_set_alarm_timer;
   if (!empty(req.params.siteId)) {
-    Events.update({
+    Events.update(
+      {
         site_set_alarm_enable: site_set_alarm_enable,
         site_set_alarm_high: site_set_alarm_high,
         site_set_alarm_low: site_set_alarm_low,
         site_set_alarm_timer: site_set_alarm_timer,
-      }, {
+      },
+      {
         where: {
           uid: req.params.userId,
           sid: req.params.siteId,
         },
-      })
+      }
+    )
       .then((result) => {
         res.json(result);
       })
@@ -890,11 +921,11 @@ mark
 // 관수 펌프 제어 상태 조회
 router.get("/:userId/site/:siteId/controls/pumps", async (req, res, next) => {
   Pumps.findAll({
-      where: {
-        uid: req.params.userId,
-        sid: req.params.siteId,
-      },
-    })
+    where: {
+      uid: req.params.userId,
+      sid: req.params.siteId,
+    },
+  })
     .then((result) => {
       res.json(result);
     })
@@ -913,16 +944,19 @@ router.put(
     let pump_type = req.body.pump_type;
     let pump_name = req.body.pump_name;
     if (!empty(req.params.pumpId)) {
-      Pumps.update({
+      Pumps.update(
+        {
           pump_type: pump_type,
           pump_name: pump_name,
-        }, {
+        },
+        {
           where: {
             uid: req.params.userId,
             sid: req.params.siteId,
             pump_id: req.params.pumpId,
           },
-        })
+        }
+      )
         .then((result) => {
           res.json(result);
         })
@@ -949,11 +983,11 @@ router.get(
   "/:userId/site/:siteId/controls/actuators",
   async (req, res, next) => {
     Actuators.findAll({
-        where: {
-          uid: req.params.userId,
-          sid: req.params.siteId,
-        },
-      })
+      where: {
+        uid: req.params.userId,
+        sid: req.params.siteId,
+      },
+    })
       .then((result) => {
         res.json(result);
       })
@@ -973,16 +1007,19 @@ router.put(
     let acturator_type = req.body.acturator_type;
     let acturator_name = req.body.acturator_name;
     if (!empty(req.params.actuatorId)) {
-      Actuators.update({
+      Actuators.update(
+        {
           acturator_type: acturator_type,
           acturator_name: acturator_name,
-        }, {
+        },
+        {
           where: {
             uid: req.params.userId,
             sid: req.params.siteId,
             motor_id: req.params.actuatorId,
           },
-        })
+        }
+      )
         .then((result) => {
           res.json(result);
         })
@@ -1010,16 +1047,19 @@ router.put(
   async (req, res, next) => {
     let motor_name = req.body.motor_name;
     if (!empty(req.params.siteId)) {
-      Motors.update({
+      Motors.update(
+        {
           motor_name: motor_name,
           // motor_id: req.params.motorId
-        }, {
+        },
+        {
           where: {
             uid: req.params.userId,
             sid: req.params.siteId,
             motor_type: "top",
           },
-        })
+        }
+      )
         .then((result) => {
           res.json(result);
         })
@@ -1047,18 +1087,21 @@ router.put(
   async (req, res, next) => {
     let motor_name = req.body.motor_name;
     if (!empty(req.params.motorId)) {
-      Motors.update({
+      Motors.update(
+        {
           // motor_type: motor_type,
           motor_name: motor_name,
           // motor_id: req.params.motorId,
-        }, {
+        },
+        {
           where: {
             uid: req.params.userId,
             sid: req.params.siteId,
             motor_id: req.params.motorId,
             motor_type: "top",
           },
-        })
+        }
+      )
         .then((result) => {
           res.json(result);
         })
