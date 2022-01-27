b05bb7de88564cafd4497913f96ea154dfe02a05
HANW001 <mosadi@nate.com>
2022-01-27 17:30:47 +0900

    edit server IP

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9080;
 const cors = require("cors");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
